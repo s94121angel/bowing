4f64adf88f6703efa8c6c52b64ec69109d3aaf4d
s94121angel <71261584+s94121angel@users.noreply.github.com>
2023-11-10 09:50:37 +0800

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,58 @@
+﻿namespace Bowling
+{
+	public class Game
+	{
+		int m_nCurrentRoll = 0;
+		int[] m_nRollsArray = new int[ 20 ];
+		const int ALL_frame = 10;
+
+		public void roll( int nPins )
+		{
+			//m_nScore += nPins;
+			m_nRollsArray[ m_nCurrentRoll ] = nPins;
+			m_nCurrentRoll++;
+		}
+
+		public int score()
+		{
+			int nScore = 0;
+			int nFrameIndex = 0;
+			for( int frame = 0; frame < ALL_frame; frame++ ) {
+				if( isStrike( nFrameIndex ) ) {
+					nScore = nScore + 10 + m_nRollsArray[ nFrameIndex + 1 ] + m_nRollsArray[ nFrameIndex + 2];
+					nFrameIndex++;
+					continue;
+				}
+
+				if( isSpare( nFrameIndex ) ) {
+
+					nScore = nScore + 10 + m_nRollsArray[ nFrameIndex + 2 ];
+
+				}
+				else {
+					nScore = nScore + m_nRollsArray[ nFrameIndex ] + m_nRollsArray[ nFrameIndex + 1 ];
+				}
+				nFrameIndex = nFrameIndex + 2;
+			}
+			return nScore;
+		}
+
+		bool isSpare( int nFrameIndex )
+		{
+			int nFramePins = m_nRollsArray[ nFrameIndex ] + m_nRollsArray[ nFrameIndex + 1 ];
+			if( nFramePins != 10 ) {
+				return false;
+			}
+			return true;
+		}
+
+		bool isStrike( int nFrameIndex )
+		{
+			int nFramePins = m_nRollsArray[ nFrameIndex ];
+			if( nFramePins != 10 ) {
+				return false;
+			}
+			return true;
+		}
+	}
+}
---
+++ Test_Game.cs
@@ -0,0 +1,149 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowling
+{
+	[TestFixture]
+	public class Test
+	{
+		[Test]
+		public void GetScore_AllZero_0Score()
+		{
+			// Arrange
+			Game g = new Game();
+
+			// Act
+			RollRepeat( g, 20, 0 );
+
+			// Assert
+			Assert.AreEqual( 0, g.score() );
+		}
+
+		[Test]
+		public void GetScore_AllOne_20Score()
+		{
+			// Arrange
+			Game g = new Game();
+
+			// Act
+			RollRepeat( g, 20, 1 );
+
+			// Assert
+			Assert.AreEqual( 20, g.score() );
+		}
+
+		[Test]
+		public void GetScore_SpareAdd3_16Score()
+		{
+			// Arrange
+			Game g = new Game();
+
+			// Act
+			g.roll( 5 );
+			g.roll( 5 );
+			g.roll( 3 );
+			RollRepeat( g, 17, 0 );
+
+			// Assert
+			Assert.AreEqual( 16, g.score() );
+		}
+
+		[Test]
+		public void GetScore_SpareInDiffFrame_Score()
+		{
+			// Arrange
+			Game g = new Game();
+
+			// Act
+			g.roll( 0 );
+			g.roll( 10 );
+			g.roll( 3 );
+			g.roll( 6 );
+			g.roll( 4 );
+			g.roll( 6 );
+			g.roll( 8 );
+			RollRepeat( g, 13, 0 );
+
+			// Assert
+			Assert.AreEqual( 48, g.score() );
+		}
+
+		[Test]
+		public void GetScore_SpareInDiffFrame_Score2()
+		{
+			// Arrange
+			Game g = new Game();
+
+			// Act
+			g.roll( 0 );
+			g.roll( 10 );
+			g.roll( 10 );
+			g.roll( 3 );
+			g.roll( 6 );
+			g.roll( 4 );
+			g.roll( 6 );
+			g.roll( 8 );
+			RollRepeat( g, 11, 0 );
+
+			// Assert
+			Assert.AreEqual( 74, g.score() );
+		}
+		
+		[Test]
+		public void GetScore_SpareInDiffFrame_Score4()
+		{
+			// Arrange
+			Game g = new Game();
+
+			// Act
+			g.roll( 10 );
+			g.roll( 2 );
+			g.roll( 3 );
+			g.roll( 1 );
+			g.roll( 9 );
+			g.roll( 6 );
+			g.roll( 3 );
+			g.roll( 2 );
+
+			RollRepeat( g, 11, 0 );
+
+			// Assert
+			Assert.AreEqual( 47, g.score() );
+		}
+
+		[Test]
+		public void GetScore_SpareInDiffFrame_Score5()
+		{
+			// Arrange
+			Game g = new Game();
+
+			// Act
+			g.roll( 10 );
+			g.roll( 10 );
+			g.roll( 10 );
+			g.roll( 10 );
+			g.roll( 10 );
+			g.roll( 10 );
+			g.roll( 10 );
+			g.roll( 10 );
+			g.roll( 10 );
+			g.roll( 10 );
+			g.roll( 10 );
+			g.roll( 10 );
+
+			// Assert
+			Assert.AreEqual( 300, g.score() );
+		}
+
+		static void RollRepeat( Game g, int nRepTimes, int nPins )
+		{
+			for( int i = 0; i < nRepTimes; i++ ) {
+				g.roll( nPins );
+			}
+		}
+	}
+}
